74d6244e1b1b8162f60c7a21e4cbbd4533f1a030
crypto427 <asaadshahmobile@gmail.com>
2026-07-13 19:46:55 -0400

    Implemented deleteList function

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -21,7 +21,7 @@ int init_linked_list(LinkedList* list) {
 }
 
 int add(void* item, LinkedList* list) {
-    Node *node = malloc(sizeof(Node)); // todo this needs to be deallocated later
+    Node *node = malloc(sizeof(Node));
     node->data = item;
     node->next = NULL;
     if (list->size == 0) {
@@ -36,11 +36,25 @@ int add(void* item, LinkedList* list) {
     return 0;
 }
 
+int deleteList(LinkedList* list) {
+    Node *on = list->head;
+    Node *next;
+    while(on != NULL){
+         next = on->next;
+         free(on);
+         on = next;
+         list->size--;
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
     printf("%s\n", argv[0]);
     LinkedList list;
     init_linked_list(&list);
     add((void*)1, &list);
+    printf("LinkedList size: %d\n", list.size);
+    deleteList(&list);
     printf("LinkedList size: %d\n", list.size);
     return 0;
 }
