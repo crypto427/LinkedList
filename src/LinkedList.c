a761a86ee1fe741d9d75fbce3452eb6c070928a7
crypto427 <asaadshahmobile@gmail.com>
2026-07-13 19:10:42 -0400

    Implemented init linked list and add functions

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -1,9 +1,10 @@
 /* LinkedList.c */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node {
-    int data;
+    void *data;
     struct Node *next;
 } Node;
 
@@ -12,8 +13,34 @@ typedef struct LinkedList {
   Node *head, *tail;
 } LinkedList;
 
-int main(int argc, char* argv[])
-{
+int init_linked_list(LinkedList* list) {
+    list->size = 0;
+    list->head = NULL;
+    list->tail = NULL;
+    return 0;
+}
+
+int add(void* item, LinkedList* list) {
+    Node *node = malloc(sizeof(Node)); // todo this needs to be deallocated later
+    node->data = item;
+    node->next = NULL;
+    if (list->size == 0) {
+    	list->head = node;
+    }
+    else {
+    	list->tail->next = node;
+    	printf("Here else.");
+    }
+    list->tail = node;
+    list->size++;
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
     printf("%s\n", argv[0]);
+    LinkedList list;
+    init_linked_list(&list);
+    add((void*)1, &list);
+    printf("LinkedList size: %d\n", list.size);
     return 0;
 }
