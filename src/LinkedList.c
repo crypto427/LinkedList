6724105731fbdc5d8fdbc6a645e8b0de8ee51892
crypto427 <asaadshahmobile@gmail.com>
2026-07-15 13:31:20 -0400

    Implemented printList and added a malloc failure check

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -13,14 +13,14 @@ typedef struct LinkedList {
   Node *head, *tail;
 } LinkedList;
 
-int init_linked_list(LinkedList* list) {
+int init_linked_list(LinkedList *list) {
     list->size = 0;
     list->head = NULL;
     list->tail = NULL;
     return 0;
 }
 
-int add(void* item, LinkedList* list) {
+int add(void *item, LinkedList *list) {
     Node *node = malloc(sizeof(Node));
     node->data = item;
     node->next = NULL;
@@ -36,11 +36,24 @@ int add(void* item, LinkedList* list) {
     return 0;
 }
 
-int deleteList(LinkedList* list) {
+Node *get(int index, LinkedList *list) {
+    Node *on = list->head;
+    int i = 0;
+    while(i < index) {
+        on = on->next;
+        i++;
+    }
+    if(on != NULL)
+        printf("%p\n", on->data);
+    return on;
+}
+
+int deleteList(LinkedList *list) {
     Node *on = list->head;
     Node *next;
     while(on != NULL){
          next = on->next;
+         free(on->data);
          free(on);
          on = next;
          list->size--;
@@ -48,12 +61,31 @@ int deleteList(LinkedList* list) {
     return 0;
 }
 
+void printList(LinkedList *list) {
+    Node *on = list->head;
+    printf("Printing list:\n");
+    while(on != NULL) {
+        printf("[data: %d ]->next => ", *(int*)on->data); // Temporary, fix this immediately
+        on = on->next;
+    }
+    printf("NULL \n");
+}
+
 int main(int argc, char* argv[]) {
     printf("%s\n", argv[0]);
     LinkedList list;
     init_linked_list(&list);
-    add((void*)1, &list);
+    int *myInt = malloc(sizeof(int));
+    if(myInt == NULL) {
+        deleteList(&list);
+        return -1;
+    }
+    *myInt = 50;
+    add((void*)myInt, &list);
     printf("LinkedList size: %d\n", list.size);
+    int *retrieved = (int*)get(0, &list)->data;
+    printf("LL(0) = %d\n", *retrieved); 
+    printList(&list);
     deleteList(&list);
     printf("LinkedList size: %d\n", list.size);
     return 0;
